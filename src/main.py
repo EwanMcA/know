93f2b17bdd047bf2d60ecac7e87ab96e8f6b45c0
EwanMcA <ewan.m@hotmail.com>
2024-05-11 20:08:26 +0100

    Try page summary

--- src/main.py
+++ src/main.py
@@ -4,21 +4,20 @@ import wikipedia
 def get_random_wikipedia_article():
     """Fetch a random Wikipedia article."""
     try:
-        random_title = wikipedia.random(pages=1)
-        page = wikipedia.page(random_title)
-        summary = wikipedia.page(random_title)
-        return random_title, page.content
+        title = wikipedia.random(pages=1)
+        page = wikipedia.page(title)
+        return title, page.summary, page.content
     except wikipedia.exceptions.WikipediaException as e:
         print("Error fetching Wikipedia content:", e)
         return None
 
 
 def main():
-    title, content = get_random_wikipedia_article()
-    print(title)
+    title, summary, content = get_random_wikipedia_article()
+    print(f"title: {title}")
+    print(f"summary: {summary}")
     print(content[:1000])
 
 
 if __name__ == "__main__":
     main()
-
